d248a406f8aba86146f4f4a0d3d7c1f13768cdf8
Rishabh Pandey <rishabhpandey8092@gmail.com>
2023-11-10 17:20:40 +0530

    Locomotive JS Fixed

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function locoMotive()
     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
     
     const locoScroll = new LocomotiveScroll({
-      el: document.querySelector("body"),
+      el: document.querySelector(".container"),
       smooth: true
     });
     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
     locoScroll.on("scroll", ScrollTrigger.update);
     
     // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy(".main", {
+    ScrollTrigger.scrollerProxy(".container", {
       scrollTop(value) {
         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
       }, // we don't have to define a scrollLeft because we're only scrolling vertically.
@@ -20,7 +20,7 @@ function locoMotive()
         return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
       },
       // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-      pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
+      pinType: document.querySelector(".container").style.transform ? "transform" : "fixed"
     });
     
     // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
@@ -125,7 +125,7 @@ gsap.to(".main-sub-heading",{
     scrollTrigger:{
       trigger:".main-sub-heading",
       // markers:true,
-      scroller:"body",
+      scroller:".container",
       start:"top 70%",
       end:"top 40%",
       scrub:3,
@@ -137,7 +137,7 @@ gsap.to(".main-sub-heading",{
 //   scrollTrigger:{
 //     trigger:".main-sub-heading",
 //     markers:true,
-//     scroller:"body",
+//     scroller:".container",
 //     start:"top 30%",
 //     end:"top 10%",
 //     scrub:3,
@@ -150,7 +150,7 @@ gsap.to(".main",{
   scrollTrigger:{
     trigger:".page1",
     // markers:true,
-    scroller:"body",
+    scroller:".container",
     start:"top 70%",
     end:"top 50%",
     scrub:3,
@@ -164,7 +164,7 @@ gsap.to(".caption h3",{
   transform:"translateX(-500%)",
   scrollTrigger:{
     trigger:".caption",
-    scroller:"body",
+    scroller:".container",
     pin:true,
     // markers:true,
     start:"top 0%",
@@ -179,7 +179,7 @@ gsap.to(".page1", {
     opacity:1,
     scrollTrigger:{
        trigger:".main-sub-heading",
-       scroller:"body",
+       scroller:".container",
       //  markers:true,
        start:"bottom 30%",
        end:"bottom 5%",
